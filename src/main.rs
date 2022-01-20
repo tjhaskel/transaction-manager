1760505be7a75dd69e525ebd2d6e25518383009a
Trevan Haskell <dev@trevanhaskell.com>
2022-01-20 08:11:00 -0700

    Removed debug output

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
         let transaction_file_path = &args[1];
-
-        if let Err(e) = process_transactions(io::stdout(), transaction_file_path) {
-            println!(
-                "Could not process transactions from {}:\n\t{}",
-                transaction_file_path, e
-            );
-        }
-    } else {
-        println!("Please provide a transaction csv file input.");
+        process_transactions(io::stdout(), transaction_file_path).unwrap()
     }
 }
